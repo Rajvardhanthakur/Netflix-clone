6be5c3fce1d0ad3188d34adeb5a6c11b5bf1dfc5
Rajvardhan <rajthakur3619@gmail.com>
2020-07-13 18:57:15 +0530

    Adding movies and rows

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './App.css';
 import Row from './Row';
+import requests from './requests';
 
 function App() {
   return (
     <div className="App">
       <h1> Making clone of netflix </h1>
-      <Row title="NETFLIX ORIGINALS"/>
-      <Row title="Trending Now" />
+      <Row title="NETFLIX ORIGINALS" fetchUrl = {requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl = {requests.fetchTrending}/>
+      <Row title="Top Rated" fetchUrl = {requests.fetchTopRated}/>
+      <Row title="Action Movies" fetchUrl = {requests.fetchActionMovies}/>
+      <Row title="Comedy Movies" fetchUrl = {requests.fetchComedyMovies}/>
+      <Row title="Romance Movies" fetchUrl = {requests.fetchRomanceMovies}/>
+      <Row title="Horror Movies" fetchUrl = {requests.fetchHorrorMovies}/>
+      <Row title="Documentaries" fetchUrl = {requests.fetchDocumentaries}/>
     </div>
   );
 }
