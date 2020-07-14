5dfa4bf1d7d2ce3991564619f08210b1f2a6b80c
Rajvardhan <rajthakur3619@gmail.com>
2020-07-14 15:10:08 +0530

    Navbar updated

--- src/Nav.js
+++ src/Nav.js
@@ -18,10 +18,7 @@ function Nav() {
 
     return (
         <div className={`nav ${show && "nav_black"}`}>
-            <img
-            className="nav_logo"
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png" 
-            alt="Netflix Logo"/>
+            <h1 className="nav_logo nav_text_color">RAJFLIX</h1>
         </div>
     )
 }
